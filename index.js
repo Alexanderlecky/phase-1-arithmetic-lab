3c2a29b295ece05ad4b27aa65b21482afb5f4dc5
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-06-26 23:00:40 +0300

    Arithmetic  lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// Write your code here 
+// Multiplying variables
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;  // This will be 62
+
+// Generating a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;  // Generates a random integer between 1 and 100
+
+// Calculating the remainder
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;  // This will be 4
+
+// Finding the highest number in a set
+let numbers = [5, 10, 20, 3, 7];
+let max = Math.max(...numbers);  // This will be 20
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
